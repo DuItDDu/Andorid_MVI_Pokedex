557613079d451d927093231a4b36bba125d0c3ec
duitddu <du.it.ddu@gmail.com>
2022-07-02 17:37:44 +0900

    System UI 핸들링 추가

--- app/src/main/java/com/duitddu/app/mvi/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/duitddu/app/mvi/pokedex/ui/MainActivity.kt
@@ -3,9 +3,13 @@ package com.duitddu.app.mvi.pokedex.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import com.duitddu.app.mvi.pokedex.ui.theme.MviPokedexTheme
+import com.google.accompanist.systemuicontroller.SystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,11 +18,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MviPokedexTheme {
+                rememberSystemUiController().apply { ConfigSystemUi(this) }
+
                 Scaffold {
                     val navController = rememberNavController()
                     MainScreen(navController)
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun ConfigSystemUi(controller: SystemUiController) {
+        controller.apply {
+            setSystemBarsColor(
+                color = MaterialTheme.colors.primary
+            )
         }
     }
 }
